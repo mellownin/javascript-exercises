6bd2dbb38b04794123f86b7d3a0bb7925ea1ff8f
Mellownyn <sunnyshettigar@gmail.com>
2022-10-31 12:41:30 -0500

    Solves removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(baseArray,remvElement1, remvElement2) {
+    // Loop through array up to maximum position
+    let maxIndex = baseArray.length;
+    let removals = Array.prototype.slice.call(arguments,1);
+    for (let i = 0; i < maxIndex; i++) {
+        if (removals.includes(baseArray[i])) {
+            baseArray.splice(i,1);
+            i--;
+        }
+    }
+    return baseArray;
 };
 
 // Do not edit below this line
