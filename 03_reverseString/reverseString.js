8d2c3afc9a950574b7c8664d3f74696b3aba6776
Mellownyn <sunnyshettigar@gmail.com>
2022-10-31 11:37:26 -0500

    Solves reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Establish index to start slicing from
+    let currIndex = -1;
+    let maxStrIndex = string.length*-1;
+    let resultString = '';
 
+    // While Loop
+    while (currIndex >= maxStrIndex) {
+        resultString += string.substr(currIndex,1);
+        currIndex--;
+    }
+
+    return resultString
 };
 
 // Do not edit below this line
