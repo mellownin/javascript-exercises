4f318f2b64b3bba395277f3c40f03b851086fc35
Mellownyn <sunnyshettigar@gmail.com>
2022-10-31 11:05:38 -0500

    Solves repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,repitions) {
+    
+    if (repitions < 0) {return "ERROR";}
+    
+    let resultString = '';
+    while (repitions) {
+        resultString += string;
+        repitions--;
+    }
+    console.log(resultString)
+    return resultString;
 };
 
 // Do not edit below this line
