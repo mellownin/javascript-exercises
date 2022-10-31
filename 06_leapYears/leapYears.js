5fe45f3c7069ebff8f9872920e80c4d17502c1e0
Mellownyn <sunnyshettigar@gmail.com>
2022-10-31 14:13:18 -0500

    Solves leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(testYear) {
+    if (testYear % 400 === 0) {return true;}
+    else if (testYear % 100 === 0) {return false;}
+    else if (testYear % 4 === 0) {return true;}
+    else {return false;}
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
