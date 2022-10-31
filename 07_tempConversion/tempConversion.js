44f81f4f809ce4264f37f396cf915ca7d4420414
Mellownyn <sunnyshettigar@gmail.com>
2022-10-31 14:23:05 -0500

    Solves tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahren) {
+  let celsius = (fahren-32)*(5/9);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahren = celsius * (9/5) + 32;
+  return parseFloat(fahren.toFixed(1));
 };
 
 // Do not edit below this line
